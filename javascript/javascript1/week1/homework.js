c9b280f423675966975652679ff9cfcf7e0ed5ba
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-04-22 12:26:31 +0200

    Modify homework.js according to mentor's comments

--- javascript/javascript1/week1/homework.js
+++ javascript/javascript1/week1/homework.js
@@ -1,28 +1,28 @@
 // Age-ify (A future age calculator)
-let yearOfBirth = 1999;
-let yearFuture = 2050;
-let age = yearFuture - yearOfBirth;
+const yearOfBirth = 1999;
+const yearFuture = 2050;
+const age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in " + yearFuture);
 
 // Goodboy-Oldboy (A dog age calculator)
-let dogYearOfBirth = 2020;
-let dogYearFuture = 2027;
-let dogYear = dogYearFuture - dogYearOfBirth;
-let shouldShowResultInDogYears = true;
-if ((shouldShowResultInDogYears = true)) {
+const dogYearOfBirth = 2020;
+const dogYearFuture = 2027;
+const dogYear = dogYearFuture - dogYearOfBirth;
+const shouldShowResultInDogYears = true;
+if (shouldShowResultInDogYears) {
   console.log(
     "Your dog will be " + dogYear * 7 + " dog years old in " + dogYearFuture
   );
-} else if ((shouldShowResultInDogYears = false)) {
+} else if (!shouldShowResultInDogYears) {
   console.log(
     "Your dog will be " + dogYear + " human years old in " + dogYearFuture
   );
 }
 
 // Housey pricey (A house price estimator)
-let volumeInMetersPeter = 8 * 10 * 10;
-let gardenSizeInM2Peter = 100;
-let housePricePeter =
+const volumeInMetersPeter = 8 * 10 * 10;
+const gardenSizeInM2Peter = 100;
+const housePricePeter =
   volumeInMetersPeter * 2.5 * 1000 + gardenSizeInM2Peter * 300;
 console.log(housePricePeter);
 if (housePricePeter > 2500000) {
@@ -33,9 +33,9 @@ if (housePricePeter > 2500000) {
   console.log("Peter pays neither too much nor too little.");
 }
 
-let volumeInMetersJulia = 5 * 11 * 8;
-let gardenSizeInM2Julia = 70;
-let housePriceJulia =
+const volumeInMetersJulia = 5 * 11 * 8;
+const gardenSizeInM2Julia = 70;
+const housePriceJulia =
   volumeInMetersJulia * 2.5 * 1000 + gardenSizeInM2Julia * 300;
 console.log(housePriceJulia);
 if (housePriceJulia > 1000000) {
@@ -47,10 +47,40 @@ if (housePriceJulia > 1000000) {
 }
 
 // Ez Namey (Startup name generator) Optional
-const firstWords = ["Easy", "Awesome", "Corporate", "Innovative", "Dynamic", "Global", "Creative", "Digital", "Smart", "Future"];
-const secondWords = ["Corporation", "Solutions", "Systems", "Labs", "Hub", "Tech", "Innovations", "Network", "Builders", "World"];
+const firstWords = [
+  "Easy",
+  "Awesome",
+  "Corporate",
+  "Innovative",
+  "Dynamic",
+  "Global",
+  "Creative",
+  "Digital",
+  "Smart",
+  "Future",
+];
+const secondWords = [
+  "Corporation",
+  "Solutions",
+  "Systems",
+  "Labs",
+  "Hub",
+  "Tech",
+  "Innovations",
+  "Network",
+  "Builders",
+  "World",
+];
 const randomNumberFirst = Math.floor(Math.random() * 10);
 const randomNumberSecond = Math.floor(Math.random() * 10);
-const startupName = firstWords[randomNumberFirst] + " " + secondWords[randomNumberSecond];
-let nameCharNumber = startupName.length;
-console.log("The startup: \"" + startupName + "\" " + "contains " + nameCharNumber + " characters")
+const startupName =
+  firstWords[randomNumberFirst] + " " + secondWords[randomNumberSecond];
+const nameCharNumber = startupName.length - 1;
+console.log(
+  'The startup: "' +
+    startupName +
+    '" ' +
+    "contains " +
+    nameCharNumber +
+    " characters"
+);
