bfae9177cf465c160aee294ad6fa9a428ba3f589
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-05-16 12:48:25 +0200

    Finished last 4 part of Voice-Assistant

--- javascript/javascript1/week4/homework-js1-w4.js
+++ javascript/javascript1/week4/homework-js1-w4.js
@@ -6,6 +6,7 @@
 let userName = "";
 let toDoList = [];
 let currentToDO = "";
+const operators = ["+", "-", "*", "/", "x"];
 
 function capitalFirstChar(strOrArr) {
   return strOrArr.charAt(0).toUpperCase() + strOrArr.slice(1);
@@ -52,11 +53,111 @@ function getReply(command) {
   } else if (command.toLowerCase().includes("what is on my todo")) {
     return `You have ${toDoList.length} todos, ${toDoList.join(" and ")}`;
   }
+
+  // Date
+  else if (command.toLowerCase().includes("what day is it today")) {
+    const today = new Date();
+    const day = today.getDate();
+    const monthIndex = today.getMonth();
+    const year = today.getFullYear();
+
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const month = months[monthIndex];
+
+    return `Today is ${day}. of ${month} ${year}`;
+  }
+
+  // Do Math.
+  else if (commandArr.some((word) => operators.includes(word))) {
+    const operatorIndex = commandArr.findIndex((word) =>
+      operators.includes(word)
+    );
+    if (operatorIndex !== -1) {
+      const operator = commandArr[operatorIndex];
+      const num1 = Number(commandArr.slice(operatorIndex - 1, operatorIndex));
+      const num2 = Number(
+        commandArr.slice(operatorIndex + 1, operatorIndex + 2)
+      );
+      switch (operator) {
+        case "+":
+          return num1 + num2;
+        case "-":
+          return num1 - num2;
+        case "*":
+        case "x":
+          return num1 * num2;
+        case "/":
+          if (num2 === 0) {
+            return "You cannot divide a number by zero";
+          }
+          return (num1 / num2).toFixed(2);
+        default:
+          return "Invalid Operator";
+      }
+    }
+    return "Not found any operator in your question";
+  }
+
+  // Timer
+  else if (command.toLowerCase().includes("set a timer for")) {
+    const forIndex = commandArr.findIndex((word) => word === "for");
+    if (forIndex !== -1 && forIndex < commandArr.length - 1) {
+      const time = parseInt(commandArr[forIndex + 1]);
+      if (
+        command.toLowerCase().includes("minute") ||
+        command.toLowerCase().includes("minutes") ||
+        command.toLowerCase().includes("min")
+      ) {
+        setTimeout(() => {
+          console.log("Timer done");
+        }, time * 60 * 1000); // Convert minutes to milliseconds
+        return `Timer set for ${time} minutes`;
+      } else if (
+        command.toLowerCase().includes("second") ||
+        command.toLowerCase().includes("seconds") ||
+        command.toLowerCase().includes("s")
+      ) {
+        setTimeout(() => {
+          console.log("Timer done");
+        }, time * 1000); // Convert seconds to milliseconds
+        return `Timer set for ${time} seconds`;
+      } else if (
+        command.toLowerCase().includes("hour") ||
+        command.toLowerCase().includes("hours") ||
+        command.toLowerCase().includes("h")
+      ) {
+        setTimeout(() => {
+          console.log("Timer done");
+        }, time * 3600 * 1000); // Convert seconds to milliseconds
+        return `Timer set for ${time} hours`;
+      } else {
+        return "Sorry, I couldn't recognize the time unit in your command.";
+      }
+    }
+    return "Invalid timer command";
+  }
+
+  // Define a Word
+  else if (command.toLowerCase().includes("define")) {
+    const keyWord = command.substring(7);
+    const wikiLink = `https://en.wikipedia.org/wiki/${keyWord}`;
+    return `The definition of '${keyWord}' can be found here: ${wikiLink}`;
+  }
   return "I'm sorry, I couldn't understand your command.";
 }
-
-// Date
-
 
 console.log(getReply("Hello my name is Benjamin")); // "Nice to meet you benjamin"
 console.log(getReply("What is my name?")); // "Your name is Benjamin"
@@ -64,3 +165,9 @@ console.log(getReply("Add fishing to my todo")); // "Fishing added to your todo"
 console.log(getReply("Add singing in the shower to my todo")); // "Singing in the shower added to your todo"
 console.log(getReply("What is on my todo?")); // "You have 2 todos, fishing and singing in the shower"
 console.log(getReply("Add fishing to my todo")); // "You have already add Singing in the shower to your todo list"
+console.log(getReply("What day is it today?")); //
+console.log(getReply("What is 3 + 3")); // 6
+console.log(getReply("What is 10 / 3")); // 3.33
+console.log(getReply("What is 4 * 12")); // 48
+console.log(getReply("Set a timer for 2 seconds")); // Will logout "Timer done" after 2 seconds.
+console.log(getReply("Define HPLC"));
