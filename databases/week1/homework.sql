5247e16cda05f113866d0a35a0752859ac5d5d8c
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-07-01 16:37:15 +0200

    done first 5 tasks

---
+++ databases/week1/homework.sql
@@ -0,0 +1,24 @@
+-- 1. Find out how many tasks are in the task table
+--  Answer: 35
+SELECT COUNT(*) AS tasks_amount FROM task;
+
+-- 2. Find out how many tasks in the task table do not have a valid due date
+--  Answer: 8
+SELECT COUNT(*) AS tasks_with_valid_due_amount FROM task WHERE due_date IS NULL;
+
+-- 3. Find all the tasks that are marked as done
+SELECT task.title AS done_tasks FROM task WHERE status_id = (SELECT id FROM status WHERE name='done'); -- 12 tasks
+
+-- 4. Find all the tasks that are not marked as done
+SELECT task.title AS not_done_tasks FROM task WHERE status_id != (SELECT id FROM status WHERE name='done'); -- 23 tasks
+
+-- 5. Get all the tasks, sorted with the most recently created first
+SELECT * FROM task ORDER BY created DESC;
+
+-- 6. Get the single most recently created task
+
+
+-- 7. Get the title and due date of all tasks where the title or description contains database
+-- 8. Get the title and status (as text) of all tasks
+-- 9. Get the name of each status, along with a count of how many tasks have that status
+-- 10.Get the names of all statuses, sorted by the status with most tasks first
