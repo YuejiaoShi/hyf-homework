1557d45c9315904fcb05d8c37e4df05d416a4d9e
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-04 15:17:11 +0200

    check if the arguments given is valid

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -2,6 +2,10 @@ import yargs from "yargs";
 
 const argv = yargs(process.argv.slice(2)).argv;
 const length = argv._.length;
-
+if (length <= 0) {
+  console.log('Plz provide some arguments after "node avg.js".');
+} else if (argv._.map((item) => typeof item !== 'number')) {
+  console.log("Plz provide numbers as arguments");
+}
 
 console.log(length);
