00e6a92aa8cb04fac07ea936155af53781a02ea7
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-04 15:30:13 +0200

    fix isNumber statements; finish the average calculation

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,11 +1,19 @@
 import yargs from "yargs";
 
 const argv = yargs(process.argv.slice(2)).argv;
-const length = argv._.length;
-if (length <= 0) {
-  console.log('Plz provide some arguments after "node avg.js".');
-} else if (argv._.map((item) => typeof item !== 'number')) {
-  console.log("Plz provide numbers as arguments");
-}
 
-console.log(length);
+function avg(argv) {
+  const numbers = argv._.map(Number);
+  const length = argv._.length;
+
+  if (length <= 0) {
+    return 'Plz provide some arguments after "node avg.js".';
+  } else if (numbers.some(isNaN)) {
+    return "Plz provide numbers as arguments";
+  } else {
+    const sum = argv._.reduce((pre, cur) => pre + cur);
+    const average = (sum / length).toFixed(2);
+    return `Tha average is: ${average}.`;
+  }
+}
+console.log(avg(argv));
