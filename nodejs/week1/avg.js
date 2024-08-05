3d376f43f1558f23539d9f9753206c1860f0c552
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-05 12:17:51 +0200

    replace yargs with process.argv for command-line argument parsing

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,19 +1,17 @@
-import yargs from "yargs";
+const args = process.argv.slice(2);
 
-const argv = yargs(process.argv.slice(2)).argv;
-
-function avg(argv) {
-  const numbers = argv._.map(Number);
-  const length = argv._.length;
+function avg(args) {
+  const numbers = args.map((arg) => parseFloat(arg));
+  const length = args.length;
 
   if (length <= 0) {
     return 'Plz provide some arguments after "node avg.js".';
   } else if (numbers.some(isNaN)) {
     return "Plz provide numbers as arguments";
   } else {
-    const sum = argv._.reduce((pre, cur) => pre + cur);
+    const sum = numbers.reduce((pre, curr) => pre + curr, 0);
     const average = (sum / length).toFixed(2);
     return `Tha average is: ${average}.`;
   }
 }
-console.log(avg(argv));
+console.log(avg(args));
