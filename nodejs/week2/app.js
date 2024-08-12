99b547ba279dbd273d86075966c6839f500a1478
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-12 17:09:38 +0200

    If q is not provided, the /search return all documents.

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { readFile } from "fs/promises";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,6 +9,22 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("This is a search engine");
+});
+
+app.get("/search", async (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    try {
+      const filePath = new URL("./documents.json", import.meta.url);
+      const contents = await readFile(filePath, { encoding: "utf8" });
+      const documents = JSON.parse(contents);
+      console.log(documents);
+
+      return res.json(documents);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
 });
 
 app.listen(port, () => {
