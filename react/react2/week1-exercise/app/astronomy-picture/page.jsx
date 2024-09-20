bdbea90735b08f9d9e918925eadf32d348903f3d
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-20 21:31:05 +0200

    fetch data using using Server Side Rendering (SSR)

--- react/react2/week1-exercise/app/astronomy-picture/page.jsx
+++ react/react2/week1-exercise/app/astronomy-picture/page.jsx
@@ -1,5 +1,22 @@
-function AstronomyPicture() {
-  return <div>AstronomyPicture Page</div>;
+const API_KEY = "jZ3XQ22AOCq4X1D1Qz2XWfE4I82l1C6uZETbQWn8";
+
+async function AstronomyPicture() {
+  const astronomyPictureURL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+  const response = await fetch(astronomyPictureURL);
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch AstronomyPicture data. Status: ${response.status}`
+    );
+  }
+
+  const data = await response.json();
+
+  return (
+    <div>
+      <img src={data.url}></img>
+      <p>{data.explanation}</p>
+    </div>
+  );
 }
 
 export default AstronomyPicture;
