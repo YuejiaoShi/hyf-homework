b26a70809587cdfbcf3987f42a45475a223bfad0
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-21 14:06:00 +0200

    add fields values validations

--- react/react2/week1-exercise/app/components/SignUpForm.jsx
+++ react/react2/week1-exercise/app/components/SignUpForm.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { TextField, Button } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function SignUpForm() {
   const firstNameRef = useRef(null);
@@ -9,6 +9,13 @@ function SignUpForm() {
   const emailRef = useRef(null);
   const phoneNumberRef = useRef(null);
   const router = useRouter();
+
+  const [errors, setErrors] = useState({
+    firstName: false,
+    lastName: false,
+    email: false,
+    phoneNumber: false,
+  });
 
   const refs = {
     firstName: firstNameRef,
@@ -19,6 +26,9 @@ function SignUpForm() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (!validateForm()) return;
+
     const formData = {};
     for (let key in refs) {
       formData[key] = refs[key].current.value;
@@ -29,7 +39,7 @@ function SignUpForm() {
   };
 
   const handleFocusNext = (e, nextRef) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && e.target.value) {
       e.preventDefault();
       if (nextRef && nextRef.current) {
         nextRef.current.focus();
@@ -39,8 +49,48 @@ function SignUpForm() {
     }
   };
 
+  const validateForm = () => {
+    const fieldValues = {
+      firstName: firstNameRef.current.value,
+      lastName: lastNameRef.current.value,
+      email: emailRef.current.value,
+      phoneNumber: phoneNumberRef.current.value,
+    };
+
+    const newErrors = {
+      firstName: !fieldValues.firstName ? "This field is required." : "",
+      lastName: !fieldValues.lastName ? "This field is required." : "",
+      email: "",
+      phoneNumber: "",
+    };
+
+    // email validation
+    if (!fieldValues.email) {
+      newErrors.email = "This field is required.";
+    } else {
+      newErrors.email = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(fieldValues.email)
+        ? ""
+        : "Email is not valid.";
+    }
+
+    // Phone number validation
+    if (!fieldValues.phoneNumber) {
+      newErrors.phoneNumber = "This field is required.";
+    } else {
+      newErrors.phoneNumber = /^\d{10}$/.test(fieldValues.phoneNumber)
+        ? ""
+        : "Phone number is not valid.";
+    }
+
+    setErrors(newErrors);
+    return !Object.values(newErrors).some((error) => error);
+  };
+
   return (
-    <form className="mt-10 p-6 w-3/5 bg-white shadow-md rounded-lg">
+    <form
+      className="mt-10 p-6 w-3/5 bg-white shadow-md rounded-lg"
+      onSubmit={handleSubmit}
+    >
       <div>
         <TextField
           inputRef={firstNameRef}
@@ -50,6 +100,8 @@ function SignUpForm() {
           variant="outlined"
           fullWidth
           margin="normal"
+          error={Boolean(errors.firstName)}
+          helperText={errors.firstName}
           onKeyDown={(e) => handleFocusNext(e, lastNameRef)}
         />
         <TextField
@@ -60,6 +112,8 @@ function SignUpForm() {
           variant="outlined"
           fullWidth
           margin="normal"
+          error={Boolean(errors.lastName)}
+          helperText={errors.lastName}
           onKeyDown={(e) => handleFocusNext(e, emailRef)}
         />
         <TextField
@@ -71,6 +125,8 @@ function SignUpForm() {
           variant="outlined"
           fullWidth
           margin="normal"
+          error={Boolean(errors.email)}
+          helperText={errors.email}
           onKeyDown={(e) => handleFocusNext(e, phoneNumberRef)}
         />
         <TextField
@@ -82,6 +138,8 @@ function SignUpForm() {
           variant="outlined"
           fullWidth
           margin="normal"
+          error={Boolean(errors.phoneNumber)}
+          helperText={errors.phoneNumber}
           onKeyDown={(e) => handleFocusNext(e, null)}
         />
       </div>
