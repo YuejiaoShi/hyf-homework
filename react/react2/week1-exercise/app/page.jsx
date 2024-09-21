90ffe4f06416743a4977684544191fb3a9da9284
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-21 13:17:46 +0200

    add some styles

--- react/react2/week1-exercise/app/page.jsx
+++ react/react2/week1-exercise/app/page.jsx
@@ -3,8 +3,8 @@ import SignUpForm from "./components/SignUpForm";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center">
       <SignUpForm />
-    </>
+    </div>
   );
 }
