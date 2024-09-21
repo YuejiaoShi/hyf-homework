be9ac891df4c186bcdcb862e86cb97b57d81b99b
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-21 13:30:36 +0200

    Create refs for the input fields, handle submit

--- react/react2/week1-exercise/app/components/SignUpForm.jsx
+++ react/react2/week1-exercise/app/components/SignUpForm.jsx
@@ -1,10 +1,35 @@
+"use client";
 import { TextField, Button } from "@mui/material";
+import { useRef } from "react";
 
 function SignUpForm() {
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const emailRef = useRef(null);
+  const phoneNumberRef = useRef(null);
+
+  const refs = {
+    firstName: firstNameRef,
+    lastName: lastNameRef,
+    email: emailRef,
+    phoneNumber: phoneNumberRef,
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = {};
+    for (let key in refs) {
+      formData[key] = refs[key].current.value;
+    }
+
+    console.log(formData);
+  };
+
   return (
     <form className="mt-10 p-6 w-3/5 bg-white shadow-md rounded-lg">
       <div>
         <TextField
+          inputRef={firstNameRef}
           required
           id="first-name"
           label="First Name"
@@ -13,6 +38,7 @@ function SignUpForm() {
           margin="normal"
         />
         <TextField
+          inputRef={lastNameRef}
           required
           id="last-name"
           label="Last Name"
@@ -21,6 +47,7 @@ function SignUpForm() {
           margin="normal"
         />
         <TextField
+          inputRef={emailRef}
           required
           id="email"
           label="Email"
@@ -30,6 +57,7 @@ function SignUpForm() {
           margin="normal"
         />
         <TextField
+          inputRef={phoneNumberRef}
           required
           id="phone-number"
           label="Phone Number"
@@ -40,6 +68,7 @@ function SignUpForm() {
         />
       </div>
       <Button
+        onClick={handleSubmit}
         type="submit"
         variant="contained"
         color="primary"
