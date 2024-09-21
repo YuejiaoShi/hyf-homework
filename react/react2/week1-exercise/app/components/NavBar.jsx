2bfbbd134b2fb63f43201c0ea805d3272e6392d1
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-21 12:38:42 +0200

    Add Button to navigate to different pages

--- react/react2/week1-exercise/app/components/NavBar.jsx
+++ react/react2/week1-exercise/app/components/NavBar.jsx
@@ -1,4 +1,11 @@
-import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Stack,
+  Button,
+} from "@mui/material";
 
 function NavBar() {
   return (
@@ -7,9 +14,23 @@ function NavBar() {
         <IconButton edge="start" color="inherit" aria-label="logo">
           <img src="/favicon.ico" alt="logo" width="30" height="30" />
         </IconButton>
-        <Typography variant="h6" component="div">
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           React2Week1
         </Typography>
+        <Stack direction="row" spacing={2} alignItems="center">
+          <Button color="inherit" href="/astronomy-picture">
+            Astronomy Picture
+          </Button>
+          <Button color="inherit" href="/rover-photos">
+            Rover Photos
+          </Button>
+          <Button color="inherit" href="/blogs">
+            Blogs
+          </Button>
+          <Button color="inherit" href="/epic">
+            EPIC image
+          </Button>
+        </Stack>
       </Toolbar>
     </AppBar>
   );
