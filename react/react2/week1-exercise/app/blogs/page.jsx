c17bc347797092c8bf8a328345ef385c72705b48
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-21 14:39:30 +0200

    add instructions and styles

--- react/react2/week1-exercise/app/blogs/page.jsx
+++ react/react2/week1-exercise/app/blogs/page.jsx
@@ -5,7 +5,14 @@ function Blogs() {
   const pathname = usePathname();
   return (
     <div>
-      <p>Current pathname: {pathname}</p>
+      <p className="mb-4 italic">Current pathname: {pathname}</p>
+      <p className="mb-4 text-xl">
+        Try changing the URL to something like{" "}
+        <code className="bg-gray-200 rounded border">
+          /blogs/your-blog-name
+        </code>{" "}
+        to see if you can find any blogs!
+      </p>
     </div>
   );
 }
