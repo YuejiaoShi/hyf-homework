220ceb1e007c2643b79f47eb4f914f0e2118a4e0
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-21 13:18:00 +0200

    add first name, last name, email and phone number fields

--- react/react2/week1-exercise/app/components/SignUpForm.jsx
+++ react/react2/week1-exercise/app/components/SignUpForm.jsx
@@ -1,171 +1,52 @@
-import { TextField } from "@mui/material";
+import { TextField, Button } from "@mui/material";
 
 function SignUpForm() {
   return (
-    <form className="mt-10">
+    <form className="mt-10 p-6 w-3/5 bg-white shadow-md rounded-lg">
       <div>
         <TextField
           required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
+          id="first-name"
+          label="First Name"
+          variant="outlined"
+          fullWidth
+          margin="normal"
         />
-        <TextField
-          disabled
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          slotProps={{
-            input: {
-              readOnly: true,
-            },
-          }}
-        />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          slotProps={{
-            inputLabel: {
-              shrink: true,
-            },
-          }}
-        />
-        <TextField id="outlined-search" label="Search field" type="search" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-        />
-      </div>
-      <div>
         <TextField
           required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
+          id="last-name"
+          label="Last Name"
+          variant="outlined"
+          fullWidth
+          margin="normal"
         />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          variant="filled"
-          slotProps={{
-            input: {
-              readOnly: true,
-            },
-          }}
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          variant="filled"
-          slotProps={{
-            inputLabel: {
-              shrink: true,
-            },
-          }}
-        />
-        <TextField
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
-      </div>
-      <div>
         <TextField
           required
-          id="standard-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="standard"
+          id="email"
+          label="Email"
+          type="email"
+          variant="outlined"
+          fullWidth
+          margin="normal"
         />
         <TextField
-          disabled
-          id="standard-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="standard"
-        />
-        <TextField
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="standard"
-        />
-        <TextField
-          id="standard-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          variant="standard"
-          slotProps={{
-            input: {
-              readOnly: true,
-            },
-          }}
-        />
-        <TextField
-          id="standard-number"
-          label="Number"
-          type="number"
-          variant="standard"
-          slotProps={{
-            inputLabel: {
-              shrink: true,
-            },
-          }}
-        />
-        <TextField
-          id="standard-search"
-          label="Search field"
-          type="search"
-          variant="standard"
-        />
-        <TextField
-          id="standard-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="standard"
+          required
+          id="phone-number"
+          label="Phone Number"
+          type="tel"
+          variant="outlined"
+          fullWidth
+          margin="normal"
         />
       </div>
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        className="mt-4"
+      >
+        Sign Up
+      </Button>
     </form>
   );
 }
