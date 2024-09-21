ba0ecf03228274739ca8e55ae1160347cbd9bf3b
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-21 12:49:06 +0200

    show a hamburger menu for smaller screens

--- react/react2/week1-exercise/app/components/NavBar.jsx
+++ react/react2/week1-exercise/app/components/NavBar.jsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   AppBar,
   Toolbar,
@@ -5,9 +6,42 @@ import {
   Typography,
   Stack,
   Button,
+  useMediaQuery,
+  ListItem,
+  List,
+  Drawer,
+  ListItemButton,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 function NavBar() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const drawer = (
+    <List>
+      <ListItem>
+        <ListItemButton href="/astronomy-picture">
+          Astronomy Picture
+        </ListItemButton>
+      </ListItem>
+      <ListItem>
+        <ListItemButton href="/rover-photos">Rover Photos</ListItemButton>
+      </ListItem>
+      <ListItem>
+        <ListItemButton href="/blogs">Blogs</ListItemButton>
+      </ListItem>
+      <ListItem>
+        <ListItemButton href="/epic">EPIC image</ListItemButton>
+      </ListItem>
+    </List>
+  );
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -17,20 +51,31 @@ function NavBar() {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           React2Week1
         </Typography>
-        <Stack direction="row" spacing={2} alignItems="center">
-          <Button color="inherit" href="/astronomy-picture">
-            Astronomy Picture
-          </Button>
-          <Button color="inherit" href="/rover-photos">
-            Rover Photos
-          </Button>
-          <Button color="inherit" href="/blogs">
-            Blogs
-          </Button>
-          <Button color="inherit" href="/epic">
-            EPIC image
-          </Button>
-        </Stack>
+        {isMobile ? (
+          <>
+            <IconButton color="inherit" onClick={toggleDrawer}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer}>
+              {drawer}
+            </Drawer>
+          </>
+        ) : (
+          <Stack direction="row" spacing={2}>
+            <Button color="inherit" href="/astronomy-picture">
+              Astronomy Picture
+            </Button>
+            <Button color="inherit" href="/rover-photos">
+              Rover Photos
+            </Button>
+            <Button color="inherit" href="/blogs">
+              Blogs
+            </Button>
+            <Button color="inherit" href="/epic">
+              EPIC image
+            </Button>
+          </Stack>
+        )}
       </Toolbar>
     </AppBar>
   );
