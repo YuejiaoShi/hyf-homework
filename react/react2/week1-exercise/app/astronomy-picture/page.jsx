2a47d50eacf53003203d8c112d83391ca60a66cb
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-21 14:33:39 +0200

    add some styles

--- react/react2/week1-exercise/app/astronomy-picture/page.jsx
+++ react/react2/week1-exercise/app/astronomy-picture/page.jsx
@@ -12,9 +12,13 @@ async function AstronomyPicture() {
   const data = await response.json();
 
   return (
-    <div>
-      <img src={data.url}></img>
-      <p>{data.explanation}</p>
+    <div className="max-w-4xl mx-auto p-4">
+      <img
+        src={data.url}
+        alt="Astronomy Picture of the Day"
+        className="w-full h-auto rounded-2xl shadow-lg mb-4"
+      />
+      <p className="text-gray-700 text-lg">{data.explanation}</p>
     </div>
   );
 }
