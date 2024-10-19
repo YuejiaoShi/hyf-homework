f6a26056476c48aed24ae050567826fad4c423ce
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-10-19 20:31:14 +0200

    create some links to navigate to the specific blogs

--- react/react2/week1-exercise/app/blogs/page.jsx
+++ react/react2/week1-exercise/app/blogs/page.jsx
@@ -1,17 +1,39 @@
 "use client";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 function Blogs() {
   const pathname = usePathname();
+
+  // Example blog slugs
+  const blogSlugs = ["first-blog-post", "second-blog-post", "third-blog-post"];
+
   return (
     <div>
       <p className="mb-4 italic">Current pathname: {pathname}</p>
+
       <p className="mb-4 text-xl">
-        Try changing the URL to something like
-        <code className="bg-gray-200 rounded border">
+        Try clicking one of the blog links below to explore:
+      </p>
+
+      <ul className="list-disc pl-6 mb-4">
+        {blogSlugs.map((slug) => (
+          <li key={slug} className="mb-2">
+            <Link
+              href={`/blogs/${slug}`}
+              className="text-blue-500 hover:underline"
+            >
+              {slug.replace(/-/g, " ")}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <p className="mb-4 text-lg">
+        Or manually change the URL to something like:
+        <code className="bg-gray-200 rounded border px-2">
           /blogs/your-blog-name
         </code>
-        to see if you can find any blogs!
       </p>
     </div>
   );
